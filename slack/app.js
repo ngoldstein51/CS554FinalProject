1395900fb50e2e136265dce61f96beb755da1187
StephenSzemis <szemis11@gmail.com>
2020-12-16 21:00:23 -0500

    Added simple Player Component

--- slack/app.js
+++ slack/app.js
@@ -46,7 +46,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email';
+  var scope = 'streaming user-read-private user-read-email user-modify-playback-state app-remote-control user-read-playback-state user-read-currently-playing';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
--- src/components/song.tsx
+++ src/components/song.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Player from './player';
 
 //Fake data for testing
-const fakeData:object=
-	{id:1,
-	name:"track1",
-	album:"album3"
-	}
+const fakeData: object =
+{
+	id: 1,
+	name: "track1",
+	album: "album3"
+}
 
 const getSong = async (tok: string, id: string) => {
 	try {
@@ -23,53 +25,55 @@ const getSong = async (tok: string, id: string) => {
 }
 
 
-const Song = (props:any) =>{
-    const [ loading, setLoading ] = useState<boolean>(true);
-    const [ songData, setSongData]=useState<any>([]);
-    const [ id	, setId ] = useState<number>(NaN);
+const Song = (props: any) => {
+	const [loading, setLoading] = useState<boolean>(true);
+	const [songData, setSongData] = useState<any>([]);
+	const [id, setId] = useState<number>(NaN);
 
-    //load
-    useEffect(():void => {
+	//load
+	useEffect((): void => {
 		async function fetchData() {
 			setLoading(true)
 
 			const song = await getSong(props.location.state[0].token, props.match.params.id);
-			
+
 			setSongData(song)
 			//Check if pagenum is valid and in bounds, if invalid or out of bounds goto page 0, need api call for bounds
 			setId(props.match.params.id)
 
 			setLoading(false)
-			
+
 		}
 		fetchData();
-    }, [props.match.params.id]); 
-    
+	}, [props.match.params.id]);
+
 	//Builds list of character links
 	//TODO: Make prettier, probably put everthing for each playlist into its own rectange with the album cover on the left or something
 
-    
-    if (loading) {
+	console.log(songData);
+
+	if (loading) {
 		return (
 			<div>
 				<h2>Loading....</h2>
 			</div>
-        );
-    }
+		);
+	}
 	//Build list of songs in playlist			//TODO: pagination
-	else{
-            return (
-                <div>
-                    <h1>Title</h1>
-					<p>{songData["name"]}: {id}</p>
-					<h1>Album</h1>
-					<p>{songData["album"]["name"]}</p>
-                    <br />
-                    <br />
-                </div>
-			);
-		}
+	else {
+		return (
+			<div>
+				<h1>Title</h1>
+				<p>{songData["name"]}: {id}</p>
+				<h1>Album</h1>
+				<p>{songData["album"]["name"]}</p>
+				<br />
+				<br />
+				<Player token={props.location.state[0].token} uri={songData['uri']} />
+			</div>
+		);
+	}
 
-    };
+};
 
 export default Song;
