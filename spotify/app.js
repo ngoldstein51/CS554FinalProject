f6ab1c29eef417f9b52815cec18dbd2df9b63070
Jared Bass <jaredjbass@gmail.com>
2020-12-19 17:59:32 -0500

    callback

--- spotify/app.js
+++ spotify/app.js
@@ -23,7 +23,7 @@ bluebird.promisifyAll(redis.Multi.prototype);
 
 var client_id = "e66c0a3b27c74129bc4e155e8300278e"; // Your client id
 var client_secret = "88019a73ec9e4893805bd5125db4b7f3"; // Your secret
-var redirect_uri = "http://cs554-final.herokuapp.com:" + process.env.PORT + "/callback"; // Your redirect uri
+var redirect_uri = "http://cs554-final.herokuapp.com/callback"; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
